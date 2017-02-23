ba0c52ec0d3e8c93c56d87915accac98b1c37ad3
Leonardo Collado Torres <lcollado@jhsph.edu>
2017-02-23 15:46:23 -0500

    Add shell file for running recount.bwtool with the F6_CAT data

---
+++ F6_CAT/F6CAT_rse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#$ -cwd
+#$ -N F6CAT_rse
+#$ -o ./F6CAT_rse.txt
+#$ -e ./F6CAT_rse.txt
+#$ -m e
+
+## Usage:
+# qsub F6CAT_rse.sh 
+
+bash /users/lcollado/R/x86_64-pc-linux-gnu-library/3.3.x/recount.bwtool/extdata/jhpce/run_rse.sh -r "/dcl01/lieber/ajaffe/lab/marchionni_projects/F6_CAT/F6CAT.Rdata" -s "sumsF6CAT" -c 1
