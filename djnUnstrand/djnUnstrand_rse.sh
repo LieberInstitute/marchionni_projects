30b47aa023d27bd25f753b2443adaaea81b2abe8
Leonardo Collado Torres <lcollado@jhsph.edu>
2017-10-03 10:14:52 -0400

    Start processing djUnstrand ranges

---
+++ djnUnstrand/djnUnstrand_rse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#$ -cwd
+#$ -N F6CAT_rse
+#$ -o ./djnUnstrand_rse_log.txt
+#$ -e ./djnUnstrand_rse_log.txt
+#$ -m e
+
+## Usage:
+# qsub djnUnstrand_rse.sh 
+
+bash /users/lcollado/R/x86_64-pc-linux-gnu-library/3.3.x/recount.bwtool/extdata/jhpce/run_rse.sh -r "/dcl01/lieber/ajaffe/lab/marchionni_projects/djnUnstrand/djnUnstrand.rda" -s "sumsDjnUnstrand" -c 1
