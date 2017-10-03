7dea4cc4c0619244a5f5196cb2accadc7fc42e34
Leonardo Collado Torres <lcollado@jhsph.edu>
2017-10-03 10:23:52 -0400

    Fix name of rda file

--- djnUnstrand/djnUnstrand_rse.sh
+++ djnUnstrand/djnUnstrand_rse.sh
@@ -8,4 +8,4 @@
 ## Usage:
 # qsub djnUnstrand_rse.sh 
 
-bash /users/lcollado/R/x86_64-pc-linux-gnu-library/3.3.x/recount.bwtool/extdata/jhpce/run_rse.sh -r "/dcl01/lieber/ajaffe/lab/marchionni_projects/djnUnstrand/djnUnstrand.rda" -s "sumsDjnUnstrand" -c 1
+bash /users/lcollado/R/x86_64-pc-linux-gnu-library/3.3.x/recount.bwtool/extdata/jhpce/run_rse.sh -r "/dcl01/lieber/ajaffe/lab/marchionni_projects/djnUnstrand/disjoinedByunstrand.rda" -s "sumsDjnUnstrand" -c 1
