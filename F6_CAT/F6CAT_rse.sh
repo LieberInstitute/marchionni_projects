fe3db965f9c540471ceb6c854ac9a2fbd8f5a25d
Leonardo Collado Torres <lcollado@jhsph.edu>
2017-10-23 00:23:27 -0400

    Update paths

--- F6_CAT/F6CAT_rse.sh
+++ F6_CAT/F6CAT_rse.sh
@@ -8,4 +8,4 @@
 ## Usage:
 # qsub F6CAT_rse.sh 
 
-bash /users/lcollado/R/x86_64-pc-linux-gnu-library/3.3.x/recount.bwtool/extdata/jhpce/run_rse.sh -r "/dcl01/lieber/ajaffe/lab/marchionni_projects/F6_CAT/F6CAT.Rdata" -s "sumsF6CAT" -c 1
+bash /users/lcollado/R/x86_64-pc-linux-gnu-library/3.3.x/recount.bwtool/extdata/jhpce/run_rse.sh -r "/dcl01/ajaffe/data/lab/marchionni_projects/F6_CAT/F6CAT.Rdata" -s "sumsF6CAT" -c 1
--- djnUnstrand/DjnNeg/djnNeg_rse.sh
+++ djnUnstrand/DjnNeg/djnNeg_rse.sh
@@ -8,4 +8,4 @@
 ## Usage:
 # qsub djnNeg_rse.sh 
 
-bash /users/lcollado/R/x86_64-pc-linux-gnu-library/3.3.x/recount.bwtool/extdata/jhpce/run_rse.sh -r "/dcl01/lieber/ajaffe/lab/marchionni_projects/djnUnstrand/DjnNeg/DjnNeg.Rdata" -s "sumsDjnNeg" -c 1
+bash /users/lcollado/R/x86_64-pc-linux-gnu-library/3.3.x/recount.bwtool/extdata/jhpce/run_rse.sh -r "/dcl01/ajaffe/data/lab/marchionni_projects/djnUnstrand/DjnNeg/DjnNeg.Rdata" -s "sumsDjnNeg" -c 1
